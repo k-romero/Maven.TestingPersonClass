8b83f7c4e8edc2a86e2773c643621f3c4a5ea764
k-romero <imkevinromero@gmail.com>
2020-02-14 09:27:35 -0500

    created 5 fields with getters and setters for person class

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,106 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String gender;
+    private boolean citizen;
+    private int phoneNumber;
+    private boolean lookingForWork;
+    private String educationLevel;
 
     public Person() {
+        this.name ="";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.name = "";
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender=gender;
+    }
+
+    public Person(String name, int age, String gender, boolean citizen) {
+        this.name = name;
+        this.age = age;
+        this.gender=gender;
+        this.citizen = citizen;
+    }
+
+    public Person(String name, int age, String gender, boolean citizen, int phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.gender=gender;
+        this.citizen = citizen;
+        this.phoneNumber = phoneNumber;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isCitizen() {
+        return citizen;
+    }
+
+    public void setCitizen(boolean citizen) {
+        this.citizen = citizen;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isLookingForWork() {
+        return lookingForWork;
+    }
+
+    public void setLookingForWork(boolean lookingForWork) {
+        this.lookingForWork = lookingForWork;
+    }
+
+    public String getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(String educationLevel) {
+        this.educationLevel = educationLevel;
     }
 }
